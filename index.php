808a4bf078e898b16b2091315877b0275290ecaf
Rohit <rohitkumararya2881@gmail.com>
2022-10-29 12:54:02 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Eshop</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	
+     <div class="container">
+     <div class="welcome"><a href="index.php">Eshop</a></div>
+	 <ul class ="menu">
+		
+          		<li>
+          			<a href="customer_registration.php" >Register </a>
+                  </li>
+                  <li>
+          			<a href="my_account.php">My Account </a>
+                  </li>
+                  <li>
+          			<a href="go_to_cart.php">Go to Cart </a>
+                  </li>
+                    <li>
+          			<a href="login.php">Log in </a>
+                  </li>
+</ul>
+</div>
+</header>
+        <nav>
+       <ul class="nav_1">
+            <li><a href="index.php" class= "btn noHover"><img src="pics/ELOGO.png"></a></li>
+            <li><a href="#">Home</a></li>
+			<li><a href="#">Shop</a></li>
+			<li><a href="#">My Account</a></li>
+			<li><a href="#">Shopping Cart</a></li>
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Contact Us</a></li>
+</ul>
+        </nav>
+
+
+</body>
+</html>
