b8dcc961950caa4c5fb614edfd52e7e333c34257
Rohit <rohitkumararya2881@gmail.com>
2022-10-29 21:00:07 +0530

    updated

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 </head>
 <body>
 	
-     <div class="container">
+     <div class="container">                             <!-- top navigation  bar or header -->
      <div class="welcome"><a href="index.php">Eshop</a></div>
 	 <ul class ="menu">
 		
@@ -17,10 +17,10 @@
           			<a href="customer_registration.php" >Register </a>
                   </li>
                   <li>
-          			<a href="my_account.php">My Account </a>
+          			<a href="my_account.php" class="my_account">My Account </a>
                   </li>
                   <li>
-          			<a href="go_to_cart.php">Go to Cart </a>
+          			<a href="go_to_cart.php" class="go_to_cart">Go to Cart </a>
                   </li>
                     <li>
           			<a href="login.php">Log in </a>
@@ -28,9 +28,11 @@
 </ul>
 </div>
 </header>
-        <nav>
+        <nav>                                          <!-- main navigation bar -->
        <ul class="nav_1">
-            <li><a href="index.php" class= "btn noHover"><img src="pics/ELOGO.png"></a></li>
+	                                                                                  
+            <li><a href="index.php" class= "btn noHover"><img src="pics/ELOGO.png"></a></li>        <!-- logo icon  -->
+			    
 			<div class="nav_2">
             <li><a href="#">Home</a></li>
 			<li><a href="#">Shop</a></li>
@@ -39,10 +41,11 @@
 			<li><a href="#">About Us</a></li>
 			<li><a href="#">Services</a></li>
 			<li><a href="#">Contact Us</a></li>
-</ul>
+</ul>                                                           <!--cart icon -->
       <div class="cart_icon"><a href="#">
 		<img src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-svg-png-icon-download-28.png"  alt="shopping cart " />
-	</a></div>
+</div></a>
+	
         </nav></div>
 </body>
 </html>
