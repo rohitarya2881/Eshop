23fa8cda81a086575bc99fb564ece3973a3bc589
Rohit <rohitkumararya2881@gmail.com>
2022-10-30 02:24:58 +0530

    new

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -27,25 +28,65 @@
                   </li>
 </ul>
 </div>
-</header>
+</header><div class="slide">
+<input type="checkbox" id="check">
+<label  for="check">
+<i class="fa fa-bars" id="btn"></i>
+<i class="fa fa-times" id="cancel"></i>
+<div class ="sidebar">
+	<header>Eshop</header>
+	<ul>    
+	         <li><a href="#"><i class="fa fa-home" aria-hidden="true"></i>Home</a></li>
+			 <li><a href="#"><i class="fa fa-user" aria-hidden="true"></i>My Account</a></li>
+			 <li><a href="#"><i class="fa fa-heart" aria-hidden="true"></i>Wishlist</a></li>
+			
+			 <li><a href="#"><i class="fa-brands fa-first-order"></i>Order</a></li>
+			<li><a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i>Shopping Cart</a></li>
+			<li><a href="#"><i class="fa fa-info" aria-hidden="true"></i>About Us</a></li>
+			<li><a href="#"><i class="fa fa-wrench" aria-hidden="true"></i>Services</a></li>
+			<li><a href="#"><i class ="fa fa-id-badge" aria-hidden="true"></i>Contact Us</a></li>
+    </ul>
+</div></div>
+</label>
+
+
+
+
+
+
+
+
+
+
+
         <nav>                                          <!-- main navigation bar -->
        <ul class="nav_1">
 	                                                                                  
             <li><a href="index.php" class= "btn noHover"><img src="pics/ELOGO.png"></a></li>        <!-- logo icon  -->
 			    
 			<div class="nav_2">
+
             <li><a href="#">Home</a></li>
-			<li><a href="#">Shop</a></li>
+			<li><a href="#">Order</a></li>
+			<li><a href="#">Wishlist</a></li>
 			<li><a href="#">My Account</a></li>
 			<li><a href="#">Shopping Cart</a></li>
 			<li><a href="#">About Us</a></li>
 			<li><a href="#">Services</a></li>
 			<li><a href="#">Contact Us</a></li>
-</ul>                                                           <!--cart icon -->
+			
+</ul>     
+
+
+<!--cart icon -->
       <div class="cart_icon"><a href="#">
 		<img src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-svg-png-icon-download-28.png"  alt="shopping cart " />
-</div></a>
+</div></a> 
+<form class="search" action="#">
+  <input type="text" placeholder="Search.." name="search">
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form>
 	
-        </nav></div>
+        </nav>
 </body>
 </html>
